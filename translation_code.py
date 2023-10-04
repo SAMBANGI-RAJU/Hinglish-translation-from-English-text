7d4d5b7574cc4d70d1e12d710c7be57c0ce399ab
SAMBANGI RAJU <115488085+SAMBANGI-RAJU@users.noreply.github.com>
2023-10-04 23:02:07 +0530

    Update translation_code.py

--- translation_code.py
+++ translation_code.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 
 
-#nltk.download('punkt')
-
-
+nltk.download('punkt')
+nltk.download("stopwords")
+nltk.download("tagsets")
 
 nlp = spacy.load("en_core_web_sm")
 translator = Translator()
@@ -20,7 +20,6 @@ def translate_sentence(sentence):
     for token in doc:
         # Check if the token is a noun (using spaCy's part-of-speech tagging)
         if token.pos_ == "NOUN":
-            # If it's a noun, add it as-is to the translated sentence
             translated_sentence += token.text + " "
         else:
             # If it's not a noun, try to translate it to Hindi
@@ -33,9 +32,9 @@ def translate_sentence(sentence):
 
     return translated_sentence
 
-# english_sentence = input("Enter an English sentence: ")
-# translated_result = translate_sentence(english_sentence)
-# print("Translated Sentence:", translated_result)
+ english_sentence = input("Enter an English sentence: ")
+ translated_result = translate_sentence(english_sentence)
+ print("Translated Sentence:", translated_result)
 
 
 
